032d85af0ea1ea54e0c2edf80601dd86a0e7a8d1
PetrPraveky <56596287+PetrPraveky@users.noreply.github.com>
2020-02-29 15:39:57 +0100

    Add files via upload

---
+++ java/script.js
@@ -0,0 +1,8 @@
+function openNav() {
+    document.getElementById("sidemenu").style.width = "250px";
+    
+};
+
+function closeNav() {
+    document.getElementById("sidemenu").style.width = "0px";
+}
